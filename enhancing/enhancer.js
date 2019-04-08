58273a4c5ebbb855e4dcec1d4fa5894916dafba7
Francis Chen <fncis.chen@gmail.com>
2019-04-08 13:42:16 -0700

    planning outline for enhancer code

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -6,14 +6,32 @@ module.exports = {
 };
 
 function succeed(item) {
-  return { ...item };
+  item.name = "Successful name";
+  if ((item.enhancement <= 20 && item.enhancement >= 0) || (item.durability <= 100 && item.durability >= 0 ))
+  {
+    return { ...item };
+  }
+  else {
+    item.enhancement = 20;
+    item.durability = 100;
+    return { ...item };
+  }
 }
 
 function fail(item) {
-  return { ...item };
+  if ((item.enhancement <= 20 && item.enhancement >= 0) || (item.durability <= 100 && item.durability >= 0 ))
+  {
+    item.enhancement = 0;
+    item.durability = 0;
+    return { ...item };
+  }
+  else {
+    return { ...item };
+  }
 }
 
 function repair(item) {
+  item.durability = 100; 
   return { ...item };
 }
 
