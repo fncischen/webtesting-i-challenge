19354d3bb18c58dafc53f23a1dc437ab75442847
Francis Chen <fncis.chen@gmail.com>
2019-04-08 15:02:38 -0700

    refactored enhancer to bound to conditions (less than or greater than methods)

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -3,11 +3,31 @@ const enhancer = require('./enhancer.js');
 
 // https://jestjs.io/docs/en/expect#expectvalue
 test("Test item for success", (item) => {
-    expect(enhancer.succeed(item.enhancer)).toBeGreaterThanOrEqual(0).toBeLessThanOrEqual(100);
-    expect(enhancer.succeed(item.durability)).toBeGreaterThanOrEqual(0).toBeLessThanOrEqual(20);
-})
+    expect(enhancer.succeed(item.enhancer)).toBeLessThanOrEqualTo({
+    
+        enhancement: , 
+        durability:
+
+    })
+    .toBeGreaterThanOrEqualTo({
+
+        enhancement: ,
+        durability: 
+        
+    })
+});
 
 test("Test item for failure", (item) => {
-    expect(enhancer.fail(item.enhancer)).toBeGreaterThanOrEqual(0).toBeLessThanOrEqual(100);
-    expect(enhancer.fail(item.durability)).toBeGreaterThanOrEqual(0).toBeLessThanOrEqual(20);
+    expect(enhancer.fail(item)).toBeLessThanOrEqualTo({
+    
+        enhancement: , 
+        durability:
+
+    })
+    .toBeGreaterThanOrEqualTo({
+
+        enhancement: ,
+        durability: 
+        
+    })
 })
