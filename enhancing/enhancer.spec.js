25eb6c4eba0b77683227ba514cc445deafbee9b5
Francis Chen <fncis.chen@gmail.com>
2019-04-08 14:19:03 -0700

    modified tests for enhancer success or failure

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,13 @@
 const enhancer = require('./enhancer.js');
 // test away!
+
+// https://jestjs.io/docs/en/expect#expectvalue
+test("Test item for success", (item) => {
+    expect(enhancer.succeed(item.enhancer)).toBeGreaterThanOrEqual(0).toBeLessThanOrEqual(100);
+    expect(enhancer.succeed(item.durability)).toBeGreaterThanOrEqual(0).toBeLessThanOrEqual(20);
+})
+
+test("Test item for failure", (item) => {
+    expect(enhancer.fail(item.enhancer)).toBeGreaterThanOrEqual(0).toBeLessThanOrEqual(100);
+    expect(enhancer.fail(item.durability)).toBeGreaterThanOrEqual(0).toBeLessThanOrEqual(20);
+})
